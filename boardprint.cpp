cdfdf20908b0ef14d16294b144363b2d4b6ebbba
Тарасенко Степан Геннадьевич <ip714s25@mwp06.csc.local>
2018-04-19 14:35:51 +0700

    include procedure print

---
+++ boardprint.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+void printboard(char a[9][9])
+{
+	for(int i=0;i<9;i++)
+    {
+      for(int j=0;j<9;j++)
+	{
+	  printf("%c ",a[i][j]);
+	}
+      printf("\n");
+    }
+}
