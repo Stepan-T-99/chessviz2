d3946441579b2e1187a87f8237636fb14dba0b5c
Тарасенко Степан Геннадьевич <ip714s25@mwp06.csc.local>
2018-04-19 14:40:58 +0700

    delete function scan

--- board.cpp
+++ board.cpp
@@ -1,31 +1,19 @@
 #include<stdio.h>
 #include<iostream>
 using namespace std;
-void scandate(char a[9][9],char &x0,char &y0,char &x1,char &y1,char &strk)			//ââîä !âîçìîæíûõ! äàííûõ
+void motion0(char a[9][9],char x0,char y0,char x1,char y1,char strk)	
 {
-	//scanf("%c %c %c %c %c",&x0,&y0,&strk,&x1,&y1);
-	cin>>x0>>y0>>strk>>x1>>y1;
-//	printf("%c%c%c%c%c \n",x0,y0,strk,x1,y1);
-	while(checkdate(x0,y0,x1,y1,strk)||checkstartposition(a,x0,y0,x1,y1,strk)||checkfirstposition(a,x0,y0,x1,y1,strk))
-	{
-//	scanf("%c%c%c%c%c",&x0,&y0,&strk,&x1,&y1);
-	cin>>x0>>y0>>strk>>x1>>y1;	
-	}
-}
-void motion0(char a[9][9],char x0,char y0,char x1,char y1,char strk)		// õîä -
-{
-//	printf("%c%c%c%c%c \n",x0,y0,strk,x1,y1);
 	char zdviz=0;
 	zdviz=a[-(((int)y0)-56)][((int)x0)-96];
 	a[-(((int)y0)-56)][((int)x0)-96]=a[-(((int)y1)-56)][((int)x1)-96];
 	a[-(((int)y1)-56)][((int)x1)-96]=zdviz;
 }
-int checkdate(char x0,char y0,char x1,char y1,char strk)			//ïðîâåðêà íàõîæäåíèÿ â èãðîâîì ïîëå
+int checkdate(char x0,char y0,char x1,char y1,char strk)		
 {
 	if(((int)x0>=97)&&((int)x0<=104)&&((int)x1>=97)&&((int)x1<=104)&&((int)y0>=49)&&((int)y0<=56)&&((int)y1>=49)&&((int)y1<=56)) return 0;
 	else return 1;
 }
-int	checkstartposition(char a[9][9],char x0,char y0,char x1,char y1,char strk)					//ïðîâåðêà õîäà ó÷èòûâàÿ íà÷àëüíóþ ïîçèöèþ
+int	checkstartposition(char a[9][9],char x0,char y0,char x1,char y1,char strk)			
 {
 	switch((int)a[-(((int)y0)-56)][((int)x0)-96])
 	{
@@ -44,7 +32,7 @@ int	checkstartposition(char a[9][9],char x0,char y0,char x1,char y1,char strk)		
 	default: return 1;
 	}
 }
-int	checkfirstposition(char a[9][9],char x0,char y0,char x1,char y1,char strk)					//ïðîâåðêà íà÷àëüíîé ïîçèöèè
+int	checkfirstposition(char a[9][9],char x0,char y0,char x1,char y1,char strk)				
 {
 	if(a[-(((int)y0)-56)][((int)x0)-96]!=' ')
 	{
@@ -53,7 +41,7 @@ int	checkfirstposition(char a[9][9],char x0,char y0,char x1,char y1,char strk)		
 	}
 	else return 1;
 }
-int queue(char a[9][9],char x0,char y0,char x1,char y1,char strk,int k)								//ïðîâåðêà íà î÷åðåäíîñòü õîäà ïåðâûå-ìàëåíüêèå
+int queue(char a[9][9],char x0,char y0,char x1,char y1,char strk,int k)					
 {
 	if(k%2)
 	{
--- board.h
+++ board.h
@@ -1,5 +1,4 @@
 void motion0(char a[9][9],char x0,char y0,char x1,char y1,char strk);
-void scandate(char a[9][9],char &x0,char &y0,char &x1,char &y1,char &strk);
 int checkdate(char x0,char y0,char x1,char y1,char strk);
 int	checkstartposition(char a[9][9],char x0,char y0,char x1,char y1,char strk);
 int	checkfirstposition(char a[9][9],char x0,char y0,char x1,char y1,char strk);	
